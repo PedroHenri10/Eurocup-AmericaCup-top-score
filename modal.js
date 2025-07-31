ef8dbc142fa937ff63f20178d44dcf9adfadc696
PedroHenri10 <dinhonoliver@gmail.com>
2025-07-30 22:16:55 -0300

    funcao fechar pelo icone + refatoracao

--- modal.js
+++ modal.js
@@ -1,51 +1,46 @@
 import { yearSelected, SelectedYear, selectedLanguage } from './index.js';
 import { updateLanguage } from './language.js';
 
-const openModal = document.querySelector(".open-modal");
+document.addEventListener("DOMContentLoaded", () => {
+  const openModal = document.querySelector(".open-modal");
+  const divGames = document.querySelector('.divGames');
+  const modal = document.getElementById('modal-champion');
+  const closeModal = document.querySelector('.close');
 
-const divGames = document.querySelector('.divGames');
+  function fetchCampaignData(selectedYear, selectedLanguage) {
+    fetch("campaign.json")
+      .then(response => response.json())
+      .then(data => {
+        let campanha = data.competitions[0].editions.find(edition => edition.year === SelectedYear).campaign;
 
-function fetchCampaignData(selectedYear, selectedLanguage) {
+        campanha.forEach(campaign => {
+          let boxgame = document.createElement('div');
+          divGames.appendChild(boxgame);
 
- /* console.log(selectedLanguage);*/
-
-fetch("campaign.json")
-  .then(response => response.json())
-  .then(data => {
-    
-    let campanha = data.competitions[0].editions.find(edition => edition.year === SelectedYear).campaign;
-
-    campanha.forEach( campaign => {
-        let boxgame = document.createElement('div');
-
-        divGames.appendChild(boxgame);
-
-        boxgame.innerHTML = `
+          boxgame.innerHTML = `
             <h3 class="match">${selectedLanguage === "Pt-BR" ? campaign.round : campaign.round_UK}</h3>
             <h4 class="match">
-                <img class="flag-modal" src="src/icones/${campaign.champion}.png" class"flag-campaign" alt="imagem seleção ${campaign.champion}"/>
-                <span class="goals">${campaign.goals_champions}</span>
-                ${campaign.match}
-                <span class="goals">${campaign.goals_opponent}</span>
-                <img class="flag-modal" src="src/icones/${campaign.opponent}.png" class"flag-campaign" alt="imagem seleção ${campaign.opponent}"/>
-                </h4>
-                <p class="penalty"><sub>${campaign.penalty}</sub></p>
+              <img class="flag-modal" src="src/icones/${campaign.champion}.png" alt="imagem seleção ${campaign.champion}"/>
+              <span class="goals">${campaign.goals_champions}</span>
+              ${campaign.match}
+              <span class="goals">${campaign.goals_opponent}</span>
+              <img class="flag-modal" src="src/icones/${campaign.opponent}.png" alt="imagem seleção ${campaign.opponent}"/>
+            </h4>
+            <p class="penalty"><sub>${campaign.penalty}</sub></p>
             <h5>${campaign.stadium}</h5>
-        `
-    }
-
-    )
-  }) 
-}
+          `;
+        });
+      });
+  }
 
   openModal.addEventListener("click", () => {
-    const modalId = openModal.getAttribute('data-modal');
-    const modal = document.getElementById('modal-champion');
-
     updateLanguage();
-
     modal.showModal();
-
-    fetchCampaignData(SelectedYear, selectedLanguage); 
+    fetchCampaignData(SelectedYear, selectedLanguage);
   });
-  
+
+  closeModal.addEventListener("click", () => {
+    modal.close();
+    divGames.innerHTML = "";
+  });
+});
