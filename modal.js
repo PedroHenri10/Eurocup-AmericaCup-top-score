6b5cd56b3c824725f5a80d75ee8a4d99d9daf9e1
PedroHenri10 <dinhonoliver@gmail.com>
2025-08-02 22:51:24 -0300

    animacao modal mobile

--- modal.js
+++ modal.js
@@ -15,30 +15,33 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(data => {
         let campanha = data.competitions[0].editions.find(edition => edition.year === SelectedYear).campaign;
 
-        campanha.forEach((campaign,index) => {
-          let boxgame = document.createElement('div');
-          divGames.appendChild(boxgame);
+        campanha.forEach((campaign, index) => {
+  setTimeout(() => {
+    let boxgame = document.createElement('div');
+    boxgame.classList.add('card-animado');
 
-          boxgame.innerHTML = `
-            <h3 class="match">${selectedLanguage === "Pt-BR" ? campaign.round : campaign.round_UK}</h3>
-            <h4 class="match">
-              <img class="flag-modal" src="src/icones/${campaign.champion}.png" alt="imagem seleção ${campaign.champion}"/>
-              <span class="goals">${campaign.goals_champions}</span>
-              ${campaign.match}
-              <span class="goals">${campaign.goals_opponent}</span>
-              <img class="flag-modal" src="src/icones/${campaign.opponent}.png" alt="imagem seleção ${campaign.opponent}"/>
-            </h4>
-            <p class="penalty"><sub>${campaign.penalty}</sub></p>
-            <h5>${campaign.stadium}</h5>
-          `;
+    boxgame.innerHTML = `
+      <h3 class="match">${selectedLanguage === "Pt-BR" ? campaign.round : campaign.round_UK}</h3>
+      <h4 class="match">
+        <img class="flag-modal" src="src/icones/${campaign.champion}.png" alt="imagem seleção ${campaign.champion}"/>
+        <span class="goals">${campaign.goals_champions}</span>
+        ${campaign.match}
+        <span class="goals">${campaign.goals_opponent}</span>
+        <img class="flag-modal" src="src/icones/${campaign.opponent}.png" alt="imagem seleção ${campaign.opponent}"/>
+      </h4>
+      <p class="penalty"><sub>${campaign.penalty}</sub></p>
+      <h5>${campaign.stadium}</h5>
+    `;
 
-          boxgame.classList.add('card-animado');
-        
-        const delay = (index + 1) * 300;
-        
-        boxgame.style.animationDelay = `${delay}ms`;
+    divGames.appendChild(boxgame);
 
-        });
+    boxgame.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end'
+    });
+
+  }, index * 400);
+});
       });
   }
 
